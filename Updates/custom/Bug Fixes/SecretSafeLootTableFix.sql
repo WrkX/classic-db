9e5970cabed8c9fdcfc0424e2e01f70e77e5f368
WrkX <pankratz.jonas95@gmail.com>
2023-10-28 19:57:03 +0200

    Missing "Secret Safe" Items were added

---
+++ Updates/custom/Bug Fixes/SecretSafeLootTableFix.sql
@@ -0,0 +1,3 @@
+INSERT INTO `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `comments`) VALUES (11104, 11926, 0, 1, 1, 1, 0, 'Deathdealer Breastplate');
+INSERT INTO `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `comments`) VALUES (11104, 11929, 0, 1, 1, 1, 0, 'Haunting Specter Leggings');
+INSERT INTO `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `comments`) VALUES (11104, 11920, 0, 1, 1, 1, 0, 'Wraith Scythe');
